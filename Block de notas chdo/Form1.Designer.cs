94d1c5c6750ed1f5843b29dd39c147b79c5366de
Cortis10 <aguilarcortis@gmail.com>
2026-09-12 15:40:33 -0600

    Botones de FILE estan listos junto con las pestañas

--- Block de notas chdo/Form1.Designer.cs
+++ Block de notas chdo/Form1.Designer.cs
@@ -29,8 +29,8 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.nuevaPestañaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -39,16 +39,23 @@
             this.guardarComoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cerrarPestañaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1.SuspendLayout();
-            this.tabControl1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(971, 474);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
             // 
             // tabPage1
             // 
             this.tabPage1.Controls.Add(this.richTextBox1);
-            this.tabPage1.Controls.Add(this.menuStrip1);
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -57,25 +64,15 @@
             this.tabPage1.Text = "tabPage1";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabControl1
-            // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(985, 509);
-            this.tabControl1.TabIndex = 1;
-            // 
             // menuStrip1
             // 
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(3, 3);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(971, 28);
-            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Size = new System.Drawing.Size(985, 28);
+            this.menuStrip1.TabIndex = 4;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
@@ -96,6 +93,7 @@
             this.nuevaPestañaToolStripMenuItem.Name = "nuevaPestañaToolStripMenuItem";
             this.nuevaPestañaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.nuevaPestañaToolStripMenuItem.Text = "Nueva Pestaña";
+            this.nuevaPestañaToolStripMenuItem.Click += new System.EventHandler(this.nuevaPestañaToolStripMenuItem_Click_1);
             // 
             // abrirToolStripMenuItem
             // 
@@ -109,34 +107,38 @@
             this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
             this.guardarToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
             // 
             // guardarComoToolStripMenuItem
             // 
             this.guardarComoToolStripMenuItem.Name = "guardarComoToolStripMenuItem";
             this.guardarComoToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.guardarComoToolStripMenuItem.Text = "Guardar como";
-            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.guardarComoToolStripMenuItem_Click);
+            this.guardarComoToolStripMenuItem.Click += new System.EventHandler(this.guardarComoToolStripMenuItem_Click_1);
             // 
             // cerrarPestañaToolStripMenuItem
             // 
             this.cerrarPestañaToolStripMenuItem.Name = "cerrarPestañaToolStripMenuItem";
             this.cerrarPestañaToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.cerrarPestañaToolStripMenuItem.Text = "Cerrar Pestaña";
+            this.cerrarPestañaToolStripMenuItem.Click += new System.EventHandler(this.cerrarPestañaToolStripMenuItem_Click);
             // 
             // salirToolStripMenuItem
             // 
             this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
             this.salirToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
             this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
             // 
-            // richTextBox1
+            // tabControl1
             // 
-            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.richTextBox1.Location = new System.Drawing.Point(3, 31);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(971, 446);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
+            this.tabControl1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 28);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(985, 481);
+            this.tabControl1.TabIndex = 5;
             // 
             // Form1
             // 
@@ -144,22 +146,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(985, 509);
             this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.menuStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Block de Notas";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            this.tabControl1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TabPage tabPage1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem nuevaPestañaToolStripMenuItem;
--- Block de notas chdo/Form1.cs
+++ Block de notas chdo/Form1.cs
@@ -19,28 +19,224 @@ namespace Block_de_notas_chdo
             InitializeComponent();
         }
 
-        private void guardarComoToolStripMenuItem_Click(object sender, EventArgs e)
+        private RichTextBox ObtenerRichTextBoxActual()
         {
-            SaveFileDialog ofd = new SaveFileDialog();
+            if (tabControl1.SelectedTab != null && tabControl1.SelectedTab.Controls.Count > 0)
+            {
+                if (tabControl1.SelectedTab.Controls[0] is RichTextBox rtb)
+                {
+                    return rtb;
+                }
+            }
+            return null;
+        }
 
+        private bool GuardarArchivoComo()
+        {
+            RichTextBox rtbActual = ObtenerRichTextBoxActual();
+            if (rtbActual == null) return false;
+
+            SaveFileDialog ofd = new SaveFileDialog();
             ofd.AddExtension = true;
             ofd.Filter = "Archivo de texto|*.txt";
             DialogResult res = ofd.ShowDialog();
 
             if (res == DialogResult.OK)
-                File.WriteAllText(ofd.FileName, richTextBox1.Text);
+            {
+                File.WriteAllText(ofd.FileName, rtbActual.Text);
+
+                tabControl1.SelectedTab.Text = Path.GetFileName(ofd.FileName);
+                tabControl1.SelectedTab.Tag = ofd.FileName; // Guardar ruta
+                return true;
+            }
+
+            return false;
+        }
+        private void guardar()
+        {
+            RichTextBox rtbActual = ObtenerRichTextBoxActual();
+            if (rtbActual == null) return;
+
+            // Checa si la pestaña ya está conectada a un archivo existente
+            if (tabControl1.SelectedTab.Tag != null)
+            {
+                string rutaArchivo = tabControl1.SelectedTab.Tag.ToString();
+                File.WriteAllText(rutaArchivo, rtbActual.Text);
+
+                tabControl1.SelectedTab.Text = Path.GetFileName(rutaArchivo);
+            }
+            else
+            {
+                GuardarArchivoComo();
+            }
+        }
+
+        private void nuevaPestaña()
+        {
+            TabPage nuevaPestaña = new TabPage();
+            nuevaPestaña.Text = "Sin Título*"; // Inicia con asterisco porque no se ha guardado en disco
+
+            // Crea el RichTextBox que va a ir dentro de la nueva pestaña
+            RichTextBox nuevoRichTextBox = new RichTextBox();
+            nuevoRichTextBox.Dock = DockStyle.Fill;
+            nuevoRichTextBox.BorderStyle = BorderStyle.None;
+
+            //Checa cuando el usuario escriba en esta pestaña
+            nuevoRichTextBox.TextChanged += NuevoRichTextBox_TextChanged;
+
+            // Añade el RichTextBox a la pestaña, y la pestaña al TabControl
+            nuevaPestaña.Controls.Add(nuevoRichTextBox);
+            tabControl1.TabPages.Add(nuevaPestaña);
+
+            tabControl1.SelectedTab = nuevaPestaña;
+        }
+
+        private void NuevoRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab != null && !tabControl1.SelectedTab.Text.EndsWith("*"))
+            {
+                tabControl1.SelectedTab.Text += "*";
+            }
+        }
+
+
+
+
+
+        private void guardarComoToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            GuardarArchivoComo();
         }
 
         private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            OpenFileDialog ofd = new OpenFileDialog();
+            RichTextBox rtbActual = ObtenerRichTextBoxActual();
 
+            // Por si el RichTextBox actual tiene texto, crea una nueva pestaña 
+            if (rtbActual != null && !string.IsNullOrEmpty(rtbActual.Text))
+            {
+                nuevaPestaña();
+            }
+            else if (tabControl1.SelectedTab == null)
+            {
+                nuevaPestaña();
+            }
+
+            rtbActual = ObtenerRichTextBoxActual();
+            OpenFileDialog ofd = new OpenFileDialog();
             ofd.AddExtension = true;
             ofd.Filter = "Archivo de texto|*.txt";
             DialogResult res = ofd.ShowDialog();
 
             if (res == DialogResult.OK)
-                richTextBox1.Text = File.ReadAllText(ofd.FileName);
+            {
+                rtbActual.Text = File.ReadAllText(ofd.FileName);
+                // Al abrirlo está sincronizado con el disco, va sin asterisco
+                tabControl1.SelectedTab.Text = Path.GetFileName(ofd.FileName);
+                tabControl1.SelectedTab.Tag = ofd.FileName;
+            }
+        }
+
+        private void nuevaPestañaToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            nuevaPestaña();
+        }
+
+        private void cerrarPestañaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Verifica si hay alguna pestaña abierta para poder cerrarla
+            if (tabControl1.SelectedTab == null) return;
+
+            // Si el archivo ya existía y no se modificó nada, se cierra directo.
+            if (tabControl1.SelectedTab.Text.EndsWith("*"))
+            {
+                DialogResult resultado = MessageBox.Show(
+                    "¿Deseas guardar los cambios antes de cerrar? Si no los guardas, se perderá todo.",
+                    "Guardar cambios",
+                    MessageBoxButtons.YesNoCancel,
+                    MessageBoxIcon.Warning
+                );
+
+                if (resultado == DialogResult.Yes)
+                {
+                    // Si ya tiene ruta guardada previamente, sobreescribimos directo
+                    if (tabControl1.SelectedTab.Tag != null)
+                    {
+                        RichTextBox rtbActual = ObtenerRichTextBoxActual();
+                        if (rtbActual != null)
+                        {
+                            File.WriteAllText(tabControl1.SelectedTab.Tag.ToString(), rtbActual.Text);
+                        }
+                    }
+                    else // Si es un archivo nuevo sin ruta, abrimos el "Guardar Como"
+                    {
+                        if (GuardarArchivoComo() == false)
+                        {
+                            return; // Canceló el guardado, se detiene el cierre de pestaña
+                        }
+                    }
+                }
+                else if (resultado == DialogResult.Cancel)
+                {
+                    return; // Se arrepintió, mantiene la pestaña abierta
+                }
+            }
+
+            // Remueve la pestaña seleccionada
+            tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+
+            // Si ya no quedan pestañas en el programa, cerramos la app por completo
+            if (tabControl1.TabPages.Count == 0) Close();
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (TabPage pestaña in tabControl1.TabPages)
+            {
+                RichTextBox rtb = pestaña.Controls[0] as RichTextBox;
+
+                // Si el título termina con '*', significa que hay texto nuevo o modificaciones sin guardar
+                if (pestaña.Text.EndsWith("*"))
+                {
+                    tabControl1.SelectedTab = pestaña; // Enfoca la pestaña con cambios pendientes
+
+                    DialogResult respuesta = MessageBox.Show(
+                        $"La pestaña '{pestaña.Text.Replace("*", "")}' tiene cambios sin guardar. ¿Quieres guardarla antes de salir?",
+                        "Salir del programa",
+                        MessageBoxButtons.YesNoCancel,
+                        MessageBoxIcon.Warning
+                    );
+
+                    if (respuesta == DialogResult.Yes)
+                    {
+                        // Si ya tiene archivo físico asociado, se auto-guarda directo
+                        if (pestaña.Tag != null)
+                        {
+                            guardar();
+                        }
+                        else
+                        {
+                            GuardarArchivoComo();
+                        }
+                    }
+                    else if (respuesta == DialogResult.Cancel)
+                    {
+                        e.Cancel = true; // Freno de mano: cancela el cierre total del programa
+                        return;
+                    }
+                    // Si responde 'No', el bucle continúa revisando la siguiente pestaña
+                }
+            }
         }
     }
 }
